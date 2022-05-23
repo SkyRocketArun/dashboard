4cfb77d551dcb53120305944723b5913d6f57b8f
SkyRocketArun <arun.skyrockets@gmail.com>
2022-05-23 19:03:22 +0530

    update

--- src/Pages/Logout.js
+++ src/Pages/Logout.js
@@ -1,11 +1,14 @@
 import { useAuth } from ".././AuthProvider";
+import { useNavigate } from "react-router-dom";
 export default function Logout() {
+  let history = useNavigate();
   const { auth } = useAuth();
   function logOut() {
     console.log("This is logout page ");
     console.log(auth);
     const logindata = false;
     localStorage.setItem("logindata", JSON.stringify(logindata));
+    history("/");
   }
 
   return (
